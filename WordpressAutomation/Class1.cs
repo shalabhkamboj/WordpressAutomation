5c18be6655a231615686a0999b2a92853b648817
shala <shala@DESKTOP-H6MO6FB>
2017-01-20 09:15:50 +1100

    Add project files.

---
+++ WordpressAutomation/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+
+namespace WordpressAutomation
+{
+    public class Class1
+    {
+
+        public void Go()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://www.google.com.au");
+
+        }
+
+    }
+}
---
+++ WordpressTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordpressAutomation;
+
+namespace WordpressTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var c = new Class1();
+            c.Go();
+        }
+    }
+}
